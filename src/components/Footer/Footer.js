43e53253874bc446a71100f748c4f62dd23374a1
TheodorePeng <pengningxin@163.com>
2021-10-20 09:58:09 +0800

    20211020

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -42,7 +42,7 @@ export default function Footer(props) {
             </ListItem>
             <ListItem className={classes.inlineBlock}>
               <a
-                href="mailto:theo.nxpeng@gmail.com"
+                href="mailto:wukongsolll@gmail.com"
                 className={classes.block}
                 target="_blank"
               >
